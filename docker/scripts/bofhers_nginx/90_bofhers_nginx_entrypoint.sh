58ee976b1253721216aa3989cc739d94c4b04457
Héctor Luaces <hector@luaces-novo.es>
2020-10-08 00:30:13 +0200

    slight refactor and better orchstration

---
+++ docker/scripts/bofhers_nginx/90_bofhers_nginx_entrypoint.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+attempts=0
+max_attempts=15
+readonly OK_ATTEMPTS=990
+readonly POLL_TIME=30
+
+function try_connect()
+{
+  wait_for_it.sh "${NGINX_FASTCGI_PASS}" -s -t "${POLL_TIME}" -- /bin/true
+}
+
+while [[ "${attempts}" -lt "${max_attempts}" ]];
+do
+  attempts=$((attempts + 1))
+
+  echo "Trying to connect to ${NGINX_FASTCGI_PASS}..."
+
+  if try_connect >& /dev/null;
+  then
+    attempts="${OK_ATTEMPTS}"
+  else
+    echo "Connection attempt to ${NGINX_FASTCGI_PASS} failed (#${attempts} of ${max_attempts}).."
+  fi
+done
+
+if [[ ! "${attempts}" -eq "${OK_ATTEMPTS}" ]];
+then
+  echo "Aborting connection attempt to ${NGINX_FASTCGI_PASS} after ${max_attempts} tries."
+  exit 1
+fi
+
+echo "Connection to ${NGINX_FASTCGI_PASS} successful. Booting up container..."
--- docker/scripts/bofhers_php/bofhers_php_entrypoint.sh
+++ docker/scripts/bofhers_php/bofhers_php_entrypoint.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e
-source ./docker/.env
+
+source docker/.env
 
 function _msg()
 {
   echo -e "\e[1;32m[${BOFHERS_APP_NAME}] $1\e[0"
 }
 # Create an application .env file if it doesn't exist
-if [[ ! -f ./.env ]];
+if [[ ! -f .env ]];
 then
   _msg "Generating .env file..."
   php -d variables_order=EGPCS docker/templates/environment.php > .env
@@ -16,7 +17,7 @@ else
 fi
 
 #Bring down composer dependencies, if they weren't before
-if [[ ! -d ./vendor ]];
+if [[ ! -d vendor ]];
 then
   _msg "Installing composer dependencies..."
   composer install
@@ -25,7 +26,7 @@ else
 fi
 
 # Create laravel encryption key if it is not present on the file
-if [[ ! $(grep -Eq 'APP_KEY=.{50}' ./.env) ]]; then
+if ! grep -Eq 'APP_KEY=.{50}' .env; then
   _msg "Creating encryption key..."
   php artisan key:generate
   php artisan cache:clear
@@ -34,9 +35,13 @@ else
 fi
 
 # Install laravel backpack
-if [[ ! -d ./templates ]]; then
+if [[ ! -f vendor/.backpack_installed ]];
+then
   _msg "Installing backpack..."
   php artisan backpack:base:install
+  touch vendor/.backpack_installed
+else
+  _msg "Backpack already installed, skipping."
 fi
 
 # Run php-fpm
