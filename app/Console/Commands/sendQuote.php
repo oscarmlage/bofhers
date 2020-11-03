1e13b04fdc383e8e5b2f44f403e1c1dc22500780
Héctor Luaces <hector@luaces-novo.es>
2020-11-03 01:30:37 +0100

    renaming variables for consistency
    see https://github.com/oscarmlage/bofhers/pull/30

--- app/Console/Commands/sendQuote.php
+++ app/Console/Commands/sendQuote.php
@@ -18,23 +18,23 @@ class sendQuote extends Command
      *
      * @var string
      */
-    protected $signature = 'telegram:sendquote {canal?} {tag?}';
+    protected $signature = 'telegram:sendquote {canal?} {category?}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Send random quote to a channel (tag is optional)';
+    protected $description = 'Send random quote to a channel (category is optional)';
 
     /**
      * Create a new command instance.
      *
      * @return void
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException
      */
     public function __construct()
     {
-        //$this->telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
         $this->telegram = new Telegram(
             env('TELEGRAM_BOT_TOKEN')
         );
@@ -44,18 +44,20 @@ class sendQuote extends Command
     /**
      * Execute the console command.
      *
+     * @throws \Telegram\Bot\Exceptions\TelegramSDKException
      */
     public function handle()
     {
         $canal = $this->argument('canal');
-        $tag = $this->argument('tag');
+        $category = $this->argument('category');
         $canales = TelegramCanal::where('active', 1)->get();
+
         if($canal) {
             $canales = TelegramCanal::where('name', "#".$canal)->where('active', 1)->get();
         }
 
         foreach($canales as $canal){
-            $quote = Quote::getAndMarkRandomQuoteText($canal->chat_id, $tag ?? null);
+            $quote = Quote::getAndMarkRandomQuoteText($canal->chat_id, $category ?? null);
 
             $data = [
                 'chat_id' => $canal->chat_id,
