7b36b496799ac5746214696a6e45f5f61857e3d6
Héctor Luaces <hector@luaces-novo.es>
2020-11-18 01:50:28 +0100

    fix: strictly forcing string

--- app/Http/Controllers/Telegram/Commands/AbstractCommand.php
+++ app/Http/Controllers/Telegram/Commands/AbstractCommand.php
@@ -155,7 +155,7 @@ abstract class AbstractCommand extends Command
      */
     protected function parseBofhersArguments(string $args): array
     {
-        $message = trim($args);
+        $message = (string) trim($args);
 
         if (empty($message) || empty($this->arguments_regexp)) {
             return [];
