d73247ffec77c2a7d291f03790825d118a00e212
Pedro Peña <pniaps@gmail.com>
2021-03-31 01:18:43 +0200

    list of channels in the front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,6 +61,59 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            ul#channels {
+                padding: 0;
+            }
+            ul#channels li {
+                position: relative;
+                list-style-type: none;
+                padding-left: 2.5rem;
+                margin-bottom: 0.5rem;
+                text-align: left;
+            }
+            ul#channels li a {
+                color:inherit;
+                text-decoration:none;
+            }
+            ul#channels li:before {
+                content: '';
+                display: block;
+                position: absolute;
+                left: 0;
+                top: -2px;
+                width: 5px;
+                height: 11px;
+                border-width: 0 2px 2px 0;
+                border-style: solid;
+                border-color: #f64f01;
+                transform-origin: bottom left;
+                transform: rotate(45deg);
+            }
+            ul#channels > * {
+                display: inline;
+            }
+            ul#channels > *:after {
+                content: ",";
+            }
+            ul#channels > *:last-child:after {
+                content: "";
+            }
+
+            @media (min-width: 768px) {
+                ul#channels {
+                    list-style: square;
+                    padding-left: 30px;
+                }
+                ul#channels > * {
+                    display: list-item;
+                    width: 20%;
+                    float: left;
+                }
+                ul#channels > *:after {
+                    content: "";
+                }
+            }
         </style>
     </head>
     <body>
@@ -83,7 +136,16 @@
                 <div class="title m-b-md">
                     #BOFHers
                 </div>
-
+                <ul id="channels">
+                    <li><a href="https://t.me/joinchat/AFVS0FRbn4VrpiloQlHz3A">#BOFHers</a></li>
+                    <li><a href="https://t.me/joinchat/RVmLC4BGx65ObjnK">#BOFHers Gamers</a></li>
+                    <li><a href="https://t.me/joinchat/AFVS0Fcqtx8rsf9KqoyZRA">#BOFHers Help</a></li>
+                    <li><a href="https://t.me/bofhers_lawyers">#BOFHers Lawyers</a></li>
+                    <li><a href="https://t.me/joinchat/IcvowkWiNEoyfQuyXKZEDg">#BOFHers Makers</a></li>
+                    <li><a href="https://t.me/joinchat/TOXRWaXwC-7Y8-HP">#BOFHers Mascotas</a></li>
+                    <li><a href="https://t.me/bofhers_news">#BOFHers News</a></li>
+                    <li><a href="https://t.me/bofhers_recetas">#BOFHers Recetas</a></li>
+                </ul>
             </div>
         </div>
     </body>
